e4dae852299bb8c6daf5911861c1878f43e56579
baracuda <25886371+Izzy129@users.noreply.github.com>
2024-09-20 23:20:55 -0500

    Add main.py: background color and title. Add stock data and categories

---
+++ main.py
@@ -0,0 +1,56 @@
+import sys
+import pygame
+import random
+
+screenWidth = 1280
+screenHeight = 720
+
+
+# initialze Pygame
+pygame.init()
+
+win = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption("Stock Symbol Shenanigans")
+
+# peach color
+backgroundColor = (250, 223, 173)
+
+# clock to use to track game time (fps usage)
+clock = pygame.time.Clock()
+running = True
+
+
+# font for text throughout game
+font_path = "arcadeFont.ttf"
+font_size = 24
+font = pygame.font.Font(font_path, font_size)
+
+while running:
+    for event in pygame.event.get():
+        # handles closing the game window
+        if event.type == pygame.QUIT:
+            running = False
+    # render the text
+    
+    TITLE_TEXT_COLOR = (78, 70, 55) 
+    TITLE_TEXT = font.render("Stock Symbol Shenanigans", True, TITLE_TEXT_COLOR)
+
+    
+    # for background color
+    win.fill(backgroundColor)
+
+    # blit the text surface onto main display surface
+    win.blit(TITLE_TEXT, (screenWidth // 2 - TITLE_TEXT.get_width() // 2, screenHeight // 2 - TITLE_TEXT.get_height() // 2))
+
+
+    
+    # update the display with new frame
+    pygame.display.flip()  
+    
+    # caps frame rate to 60 fps
+    clock.tick(60) # should be at the end of the game loop
+    
+    
+# quit Pygame if required
+pygame.quit()
+sys.exit()
